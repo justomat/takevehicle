3c970f659eed6e6ecb22c2f39cbff6d459ede0d1
Geraldi Sutanto <sutantogeraldi@gmail.com>
2015-07-09 19:25:29 +0700

    reformated codes and deleted txt files

--- Helper.h
+++ Helper.h
@@ -2,24 +2,20 @@
 #include <vector>
 #include <sstream>
 
-class Helper
-{
-public:
-	static std::vector<std::string>& split(const std::string& s, char delim, std::vector<std::string>& elems)
-	{
-		std::stringstream ss(s);
-		std::string item;
-		while (std::getline(ss, item, delim))
-		{
-			elems.push_back(item);
-		}
-		return elems;
-	}
+class Helper {
+  public:
+    static std::vector<std::string>& split(const std::string& s, char delim, std::vector<std::string>& elems) {
+        std::stringstream ss(s);
+        std::string item;
+        while (std::getline(ss, item, delim)) {
+            elems.push_back(item);
+        }
+        return elems;
+    }
 
-	static std::vector<std::string> split(const std::string& s, char delim)
-	{
-		std::vector<std::string> elems;
-		split(s, delim, elems);
-		return elems;
-	}
+    static std::vector<std::string> split(const std::string& s, char delim) {
+        std::vector<std::string> elems;
+        split(s, delim, elems);
+        return elems;
+    }
 };
